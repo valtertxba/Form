164019adbc3f14f88bb771fbeb8fe7b674217330
Valter <valter122415@gmail.com>
2024-03-12 22:31:34 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const body = document.body;
+const loginBox = document.querySelector(".login-box");
+const h1 = document.getElementsByTagName( "h1" ) [ 0 ];
+const inputs = document.querySelectorAll ("input");
+const loginButton = document.getElementsByID("login-button");
+
+function switchTheme(){
+    loginBox.classList.toggle ("dark-mode");
+    body.classList.toggle("dark-mode");
+    h1.classList.toggle("dark-mode");
+    inputs.forEach( input => {
+        input.classList.toggle("dark-mode");
+    } );
+    loginButton.classList.toggle("dark-mode");
+    h2.classList.toggle("dark-mode");
+}
